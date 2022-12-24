5e5506c4545eed9dda9f8d9373507b1e47fc5d1c
Alisson Ribeiro <89925320+Alisson-Ribeiro@users.noreply.github.com>
2022-12-24 19:07:44 -0300

    Update Main.java

--- exercicios_java/src/Main.java
+++ exercicios_java/src/Main.java
@@ -5,7 +5,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		//Faça um algoritmo que leia a idade de uma pessoa expressa em anos, meses e dias e mostre-a expressa em dias. Leve em consideração o ano com 365 dias e o mês com 30. (Ex: 3 anos, 2 meses e 15 dias = 1170 dias.)
+		//Faça um algoritmo que leia a idade de uma pessoa expressa em anos, meses e dias e mostre-a expressa em dias.
+		//Leve em consideração o ano com 365 dias e o mês com 30. (Ex: 3 anos, 2 meses e 15 dias = 1170 dias.)
 		
 		    Scanner sc = new Scanner(System.in);
 		    System.out.println("Digite sua idade.");
@@ -30,6 +31,10 @@ public class Main {
 		    //idade = (idade*365);
 
 		    System.out.println("Esse é o número em dias da sua idade: " + idadeEmDias);
+		
+		sc.close();
+		
+		
 
 	}
 
